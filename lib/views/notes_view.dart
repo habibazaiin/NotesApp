62ad91ff9361a51ca4fd9a6d85d6c16d739cec01
Habiba <zain.habiba0@gmail.com>
2025-08-27 19:11:20 +0300

    add note model sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/widgets/add_note_model_sheet.dart';
 import 'package:my_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,10 +9,20 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            context: context,
+            builder: (context) {
+              return const AddNoteBottomSheet();
+            },
+          );
+        },
         shape: CircleBorder(),
         backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
